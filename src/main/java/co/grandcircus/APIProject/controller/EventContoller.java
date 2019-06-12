c6fab4d8edcb652a19e2f6bd70bf8a8c06a1519a
LinmeiJ <linmeijiang@hotmail.com>
2019-06-12 11:45:19 -0400

    add controller

---
+++ src/main/java/co/grandcircus/APIProject/controller/EventContoller.java
@@ -0,0 +1,36 @@
+package co.grandcircus.APIProject.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class EventContoller {
+
+	@Value("${events.key}")
+	String eventKey;
+	
+	RestTemplate rt = new RestTemplate();
+	
+	@RequestMapping("/")
+	public ModelAndView index() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("x-api-key", eventKey);
+		headers.add("Accept",MediaType.APPLICATION_JSON_VALUE);
+		
+		
+		String url="https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey="+eventKey;
+		ResponseEntity<_Enbaded> response = rt.exchange(url,HttpMethod.GET, new HttpEntity<>("paramters", headers), _Enbaded.class);
+		
+		
+		return new ModelAndView("index");
+	}
+}
